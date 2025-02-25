ba16c4b25377bab8c920f13e2451388cf94d8d01
IanaPopova <164924290+IanaPopova@users.noreply.github.com>
2025-02-25 11:31:02 +0300

    Basics HW

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let d = b ** 2 - 4 * a * c;
+    if (d < 0) {
+        return [];
+    } else if (d === 0) {
+        return [-b / (2 * a)];
+    } else {
+     
+       return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+    }
 }
 
+console.log(solveEquation(1, -3, 2))
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    percent = parseFloat(percent);
+    contribution = parseFloat(contribution);
+    amount = parseFloat(amount);
+    countMonths = parseInt(countMonths);
+    
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+        return false;
+    }
+    
+    let monthlyRate = (percent / 100) / 12;
+    let loanBody = amount - contribution;
+    
+    let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / ((1 + monthlyRate) ** countMonths - 1)));
+    let totalPayment = (monthlyPayment * countMonths).toFixed(2);
+    
+    return Number(totalPayment);
 }
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
