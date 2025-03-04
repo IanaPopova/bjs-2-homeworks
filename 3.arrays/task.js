d301bcbc15c74c1e6d37ad1624fc1f4661bfaa5f
IanaPopova <164924290+IanaPopova@users.noreply.github.com>
2025-03-04 13:06:46 +0300

    arrays HW added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
+  function getUsersNamesInAgeRange(users, gender) {
+    let filteredUsers = users.filter(user => user.gender === gender);
+    if (filteredUsers.length === 0) return 0;
   
-}
+    let averageAge = filteredUsers
+      .map(user => user.age)
+      .reduce((sum, age) => sum + age, 0) / filteredUsers.length;
+    
+    return averageAge;
+  }
+  
